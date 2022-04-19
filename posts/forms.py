c9d914d23d8fa084aee67ba9552515288addf3a7
Winfred Adu-Acheampong <winfred242002@gmail.com>
2022-04-19 14:22:34 +0000

    refactor

--- posts/forms.py
+++ posts/forms.py
@@ -1,13 +1,10 @@
 from django import forms
-from django.forms import ModelForm
+from django.shortcuts import redirect
 from .models import Post
-    
-
-
+from django.utils import timezone
 
 
 class CreateToDo(forms.ModelForm):
-    
     class Meta:
-        model = Post 
-        fields = ['title', 'description', 'due_date', 'due_time', 'priority', 'done']
+        model = Post
+        fields = ['title', 'description', 'due_date', 'due_time', 'priority']
